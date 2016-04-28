42c4ebfa9ab4ee43f790f9216e53b612551d7fa0
mriddle <mriddle89@gmail.com>
2016-04-29 09:57:04 +1000

    Lecture 2, stuffed up input field

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,41 +1,46 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Matthew Riddle on 22/04/2016.
-//  Copyright © 2016 Matthew Riddle. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-
-    @IBOutlet weak var display: UILabel!
-    
-    var userIsInTheMiddleOfTyping = false
-    
-    @IBAction func touchDigit(sender: UIButton) {
-        let digit = sender.currentTitle!
-        if userIsInTheMiddleOfTyping {
-            let textCurrentlyInDisplay = display.text!
-            display.text = textCurrentlyInDisplay + digit
-        } else {
-            display.text = digit
-        }
-        userIsInTheMiddleOfTyping = true
-        
-
+  
+  
+  @IBOutlet private weak var display: UILabel!
+  
+  private var userIsInTheMiddleOfTyping = false
+  
+  @IBAction private func touchDigit(sender: UIButton) {
+    let digit = sender.currentTitle!
+    if userIsInTheMiddleOfTyping {
+      let textCurrentlyInDisplay = display.text!
+      display.text = textCurrentlyInDisplay + digit
+    } else {
+      display.text = digit
     }
-
-    @IBAction func performOperation(sender: UIButton) {
-        userIsInTheMiddleOfTyping = false
-        if let mathematicalSymbol = sender.currentTitle {
-            if mathematicalSymbol == "π" {
-                display.text = String(M_PI)
-            }
-        }
+    userIsInTheMiddleOfTyping = true
+  }
+  
+  private var displayValue: Double {
+    get {
+      return Double(display.text!)!
     }
-
+    set {
+      display.text = String(newValue)
+    }
+  }
+  
+  private var brain = CalculatorBrain()
+  
+  @IBAction private func performOperation(sender: UIButton) {
+    if userIsInTheMiddleOfTyping {
+      brain.setOperand(displayValue)
+      userIsInTheMiddleOfTyping = false
+    }
+    
+    if let mathematicalSymbol = sender.currentTitle {
+      brain.performOperation(mathematicalSymbol)
+      
+    }
+    displayValue = brain.result
+  }
+  
 }
 
