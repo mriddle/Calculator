e1e2aad07afa85f079c8200002031cd05c897244
mriddle <mriddle89@gmail.com>
2016-04-29 10:15:03 +1000

    Typo

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -13,17 +13,17 @@ class CalculatorBrain {
     "e": Operation.Constant(M_E),
     "√": Operation.UnaryOperation(sqrt),
     "cos": Operation.UnaryOperation(cos),
-    "×": Operation.Binaryperation({ $0 * $1 }),
-    "-": Operation.Binaryperation({ $0 - $1 }),
-    "+": Operation.Binaryperation({ $0 + $1 }),
-    "÷": Operation.Binaryperation({ $0 / $1 }),
+    "×": Operation.BinaryOperation({ $0 * $1 }),
+    "-": Operation.BinaryOperation({ $0 - $1 }),
+    "+": Operation.BinaryOperation({ $0 + $1 }),
+    "÷": Operation.BinaryOperation({ $0 / $1 }),
     "=": Operation.Equals
   ]
   
   private enum Operation {
     case Constant(Double)
     case UnaryOperation((Double) -> Double)
-    case Binaryperation((Double, Double) -> Double)
+    case BinaryOperation((Double, Double) -> Double)
     case Equals
   }
   
@@ -32,7 +32,7 @@ class CalculatorBrain {
       switch operation {
       case .Constant(let value): accumulator = value
       case .UnaryOperation(let function): accumulator = function(accumulator)
-      case .Binaryperation(let function):
+      case .BinaryOperation(let function):
         executePendingBinaryOperation()
         pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
       case .Equals:
